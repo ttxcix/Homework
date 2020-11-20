754491ee6c0660c9e51be8a51557c4ca45b7be73
ttxcix <71488910+ttxcix@users.noreply.github.com>
2020-11-20 19:53:13 +0800

    Add files via upload

---
+++ 11-20.js
@@ -0,0 +1,19 @@
+var dun = prompt("Suragch N1 - N5 hurtel songono uu")
+
+switch(dun){
+    case "N1":
+        console.log("Mash muu");
+        break
+    case "N2":
+        console.log("Dund")
+        break
+    case "N3":
+        console.log("Sain")
+        break
+    case "N4":
+        console.log("Mash sain")
+        break
+    case "N5":
+        console.log("Muu")
+        break
+}
